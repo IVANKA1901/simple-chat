00b7a0fc53c060b1a1807e04bdb46671a3754e53
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-25 13:42:39 +0200

    update

---
+++ public/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+
+const { PORT } = process.env;
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(PORT || 3001, () => {
+  console.log("Server running on port 3001");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+const users: { [key: string]: string } = {};
+
+const io = new Server(server);
+
+io.on("connection", (client: Socket) => {
+  const broadcast = (event: string, data: any) => {
+    client.emit(event, data);
+    client.broadcast.emit(event, data);
+  };
+
+  broadcast("users", users);
+
+  client.on("message", (message: { name: string; text?: string }) => {
+    if (users[client.id] !== message.name) {
+      users[client.id] = message.name;
+      broadcast("user", users);
+    }
+    broadcast("message", message);
+  });
+});
